19a57940cf3936e4cdf28b4d0b52a21b7d6d86b2
Thomas VANDEVELDE <thomas.vandevelde@digital.hmrc.gov.uk>
2017-12-15 16:25:53 +0000

    Add content

--- app/controllers/ScapigController.scala
+++ app/controllers/ScapigController.scala
@@ -9,9 +9,34 @@ import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
 @Singleton
-class ScapigController @Inject()(cc: ControllerComponents)(implicit webJarsUtil: WebJarsUtil) extends AbstractController(cc) {
+class ScapigController @Inject()(cc: ControllerComponents)(implicit webJarsUtil: WebJarsUtil, assets: AssetsFinder) extends AbstractController(cc) {
 
   def root() = Action.async { implicit request =>
     Future(Ok(views.html.index()))
   }
+
+  def api() = Action.async { implicit request =>
+    Future(Ok(views.html.api()))
+  }
+
+  def application() = Action.async { implicit request =>
+    Future(Ok(views.html.application()))
+  }
+
+  def authorization() = Action.async { implicit request =>
+    Future(Ok(views.html.authorization()))
+  }
+
+  def developerHub() = Action.async { implicit request =>
+    Future(Ok(views.html.developerHub()))
+  }
+
+  def gateway() = Action.async { implicit request =>
+    Future(Ok(views.html.gateway()))
+  }
+
+  def publisher() = Action.async { implicit request =>
+    Future(Ok(views.html.publisher()))
+  }
+
 }
