f2e2c33ba7763cdadf3ce2c177da8c073d4144d3
Thomas VANDEVELDE <thomas.vandevelde@digital.hmrc.gov.uk>
2018-01-21 17:30:34 +0000

    update port

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ COPY target/universal/web-*.tgz .
 COPY start-docker.sh .
 RUN chmod +x start-docker.sh
 RUN tar xvf web-*.tgz
-EXPOSE 8585
+EXPOSE 9050
 
 CMD ["sh", "start-docker.sh"]
--- start-docker.sh
+++ start-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 SCRIPT=$(find . -type f -name web)
 rm -f web*/RUNNING_PID
-exec $SCRIPT -Dhttp.port=8585 -J-Xms128M -J-Xmx512m
+exec $SCRIPT -Dhttp.port=9050 -J-Xms128M -J-Xmx512m
