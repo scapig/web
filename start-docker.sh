4aea03ab0007b58eb057eb0e857c415c57bf985a
Thomas VANDEVELDE <thomas.vandevelde@digital.hmrc.gov.uk>
2018-02-03 21:10:52 +0000

    allow configurable parameters

--- start-docker.sh
+++ start-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 SCRIPT=$(find . -type f -name web)
 rm -f web*/RUNNING_PID
-exec $SCRIPT -Dhttp.port=9050 -J-Xms16M -J-Xmx64m
+exec $SCRIPT -Dhttp.port=9050 $JAVA_OPTS -J-Xms16M -J-Xmx64m
